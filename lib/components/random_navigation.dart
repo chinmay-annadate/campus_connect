bbc30101b61556b9c27b71d2c8c742a7573ca0f1
chinmay.annadate <ccannadate@gmail.com>
2023-06-28 23:56:09 +0530

    go button + ui changes

--- lib/components/random_navigation.dart
+++ lib/components/random_navigation.dart
@@ -69,7 +69,8 @@ class _PopupMenuState extends State<PopupMenu> {
     floorsDropDownValue = widget.currentFloor;
 
     // get list of rooms from current floor
-    roomsDropDownList = (floors[floorsDropDownValue].first as List).cast<String>();
+    roomsDropDownList =
+        (floors[floorsDropDownValue].first as List).cast<String>();
 
     // set selected room to collected room
     roomsDropDownValue = widget.currentRoom;
@@ -91,122 +92,145 @@ class _PopupMenuState extends State<PopupMenu> {
       alignment: Alignment.topRight,
       child: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // Buildings dropdown
-            DropdownButton<String>(
-              value: buildingsDropDownValue,
-              items: buildingsDropDownList.map<DropdownMenuItem<String>>(
-                (String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(
-                      value,
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  );
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Buildings dropdown
+              DropdownButton<String>(
+                value: buildingsDropDownValue,
+                items: buildingsDropDownList.map<DropdownMenuItem<String>>(
+                  (String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    );
+                  },
+                ).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    buildingsDropDownValue = newValue!;
+
+                    floors = widget.buildings[buildingsDropDownValue];
+                    floorsDropDownList = floors.keys.toList().cast<String>();
+                    floorsDropDownValue = floorsDropDownList.first;
+
+                    roomsDropDownList =
+                        (floors[floorsDropDownValue].first as List)
+                            .cast<String>();
+                    roomsDropDownValue = roomsDropDownList.first;
+
+                    // set image src for current floor
+                    getFloorPlan(floors[floorsDropDownValue].last);
+                  });
                 },
-              ).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  buildingsDropDownValue = newValue!;
+              ),
 
-                  floors = widget.buildings[buildingsDropDownValue];
-                  floorsDropDownList = floors.keys.toList().cast<String>();
-                  floorsDropDownValue = floorsDropDownList.first;
+              // floors dropdown
+              DropdownButton<String>(
+                value: floorsDropDownValue,
+                items: floorsDropDownList.map<DropdownMenuItem<String>>(
+                  (String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    );
+                  },
+                ).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    floorsDropDownValue = newValue!;
+                    roomsDropDownList =
+                        (floors[floorsDropDownValue].first as List)
+                            .cast<String>();
+                    roomsDropDownValue = roomsDropDownList.first;
 
-                  roomsDropDownList =
-                      (floors[floorsDropDownValue].first as List).cast<String>();
-                  roomsDropDownValue = roomsDropDownList.first;
-
-                  // set image src for current floor
-                  getFloorPlan(floors[floorsDropDownValue].last);
-                });
-              },
-            ),
-
-            // floors dropdown
-            DropdownButton<String>(
-              value: floorsDropDownValue,
-              items: floorsDropDownList.map<DropdownMenuItem<String>>(
-                (String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(
-                      value,
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  );
+                    // set image src for current floor
+                    getFloorPlan(floors[floorsDropDownValue].last);
+                  });
                 },
-              ).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  floorsDropDownValue = newValue!;
-                  roomsDropDownList =
-                      (floors[floorsDropDownValue].first as List).cast<String>();
-                  roomsDropDownValue = roomsDropDownList.first;
+              ),
 
-                  // set image src for current floor
-                  getFloorPlan(floors[floorsDropDownValue].last);
-                });
-              },
-            ),
+              // rooms drop down + go button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  DropdownButton<String>(
+                    value: roomsDropDownValue,
+                    items: roomsDropDownList.map<DropdownMenuItem<String>>(
+                      (String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        roomsDropDownValue = newValue!;
+                      });
+                    },
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        Navigator.pop(context);
+                        widget.updateCurrent(roomsDropDownValue);
+                      });
+                    },
+                    icon: Icon(Icons.refresh),
+                    label: Text('Go'))
+                ],
+              ),
 
-            // rooms dropdown
-            DropdownButton<String>(
-              value: roomsDropDownValue,
-              items: roomsDropDownList.map<DropdownMenuItem<String>>(
-                (String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(
-                      value,
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  );
-                },
-              ).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  roomsDropDownValue = newValue!;
-                  Navigator.pop(context);
-                  widget.updateCurrent(roomsDropDownValue);
-                });
-              },
-            ),
-
-            // floor plan image
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Scaffold(
-                      body: SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height,
-                        child: PhotoView(
-                          minScale: PhotoViewComputedScale.contained,
-                          imageProvider: CachedNetworkImageProvider(floorPlan),
-                          backgroundDecoration: const BoxDecoration(
-                            color: Colors.black,
+              // sized box
+              const SizedBox(
+                height: 20,
+              ),
+              
+              // floor plan image
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Scaffold(
+                        body: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height,
+                          child: PhotoView(
+                            minScale: PhotoViewComputedScale.contained,
+                            imageProvider:
+                                CachedNetworkImageProvider(floorPlan),
+                            backgroundDecoration: const BoxDecoration(
+                              color: Colors.black,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
-              child: CachedNetworkImage(
-                imageUrl: floorPlan,
-                placeholder: (context, url) =>
-                    const CircularProgressIndicator(),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
-              ),
-            )
-          ],
+                  );
+                },
+                child: CachedNetworkImage(
+                  imageUrl: floorPlan,
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) =>
+                      const Icon(Icons.error),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
